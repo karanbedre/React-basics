17723edf510e5ba6c4d21d3c466be04adb1defa9
karan <karan.d@mobinius.com>
2020-04-12 21:07:59 +0530

    completed home page with broken components, stylings

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component'
-class App extends Component {
-  constructor() {
-    super();
+import HomePage from './pages/home-page/home-page.component';
 
-    this.state = {
-      userArray: [],
-      searchField: ''
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users').
-      then(res => res.json())
-      .then(users => {
-        this.setState({ userArray: users })
-      });
-
-  }
-
-  handleChange = event =>{
-    this.setState({ searchField: event.target.value })
-  }
-
-  
-  render() {
-    const { userArray, searchField } = this.state;
-
-    const filteredUserArray = userArray.filter(userList =>
-      userList.name.toLowerCase().includes(searchField.toLowerCase()));
-
-    return (
-      <div className="App">
-        <h1>React Basics</h1>
-        <SearchBox placeholder={'Search Users'} handleChange={this.handleChange} />
-        <CardList users={filteredUserArray} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
 }
 
 export default App;

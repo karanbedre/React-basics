2c0a8e34d9af2435bcf0321adaa27ed1b501de9c
karan <karan.d@mobinius.com>
2020-04-20 16:55:04 +0530

    feat:shop-page with collection preview and colletiomn item components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home-page/home-page.component';
+import ShopPage from './pages/shop-page/shop-page.component'
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
-  )
+  );
 }
-
 export default App;
